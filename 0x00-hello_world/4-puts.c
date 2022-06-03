5a34942324951c699ec9cee920c1bce5f6abf365
Whitzypro <nonsowisdom62@gmail.com>
2022-06-02 21:01:33 -0700

    Betty Coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
-	/* Print string with puts */
-	puts("Programming is like building a multilingual puzzle\n");
+	/**
+	 *  Print string with puts
+	 */
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
