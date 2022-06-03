a2aa71cd7d384d79993ca3b5c47527b9bf625192
Whitzypro <nonsowisdom62@gmail.com>
2022-06-02 21:16:12 -0700

    Print with printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include <stdio.>
+/**
+ * Print with th eputs function 
+ */
 int main(void)
 {
-	/**
-	 *  Print string with puts
-	 */
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
