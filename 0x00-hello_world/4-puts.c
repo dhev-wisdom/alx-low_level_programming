3d288609f71717051398251d31204ddb637216f5
Whitzypro <nonsowisdom62@gmail.com>
2022-06-02 21:36:04 -0700

    At Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * Print with th eputs function 
+ *Print with the puts function
+ *Return Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
