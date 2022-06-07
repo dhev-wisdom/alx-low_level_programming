20071f7e05813bd192e140ac4965a0e0a7e49c42
Whitzypro <nonsowisdom62@gmail.com>
2022-06-07 15:07:18 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - chech the code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
