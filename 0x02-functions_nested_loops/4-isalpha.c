9168729b67638a632b8ac215de1650a0da8435bb
Whitzypro <nonsowisdom62@gmail.com>
2022-06-11 17:41:48 -0700

    fifth commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * _isalpha - Return 1 if c is a letter, lowercase or uppercase
+ * _isalpha - Return 1 if c is lowercase or uppercase
  *
- * @c: the integer to print
- *
+ * @c: The integer to print
  * Return: Returns 0 otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c <= 65 && c >= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
-	else 
+	else
 	{
 		return (0);
 	}
