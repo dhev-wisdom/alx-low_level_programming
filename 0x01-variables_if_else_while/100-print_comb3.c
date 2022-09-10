09379770d0b45d84b3fe031d52bdf68b6d955348
dhev-wisdom <nonsowisdom62@gmail.com>
2022-09-10 13:45:28 -0700

    Correct input

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,15 +6,16 @@
  */
 int main(void)
 {
-	int x,y;
+	int x;
+	int y;
 
-	for (x = 0; x <= 8; x++)
+	for (x = '0'; x <= '8'; x++)
 	{
-		for (y = x + 1; y <= 9; y++)	
+		for (y = x + '1'; y <= '9'; y++)	
 		{
 			putchar(x);
 			putchar(y);
-			if (x == 8 && y == 9)
+			if (x == '8' && y == '9')
 				continue;
 			putchar(',');
 			putchar(' ');
