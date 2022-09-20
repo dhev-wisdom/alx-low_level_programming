dc0c791b2e6dc0e9a5fb7871bac034d5b5dea38b
dhev-wisdom <nonsowisdom62@gmail.com>
2022-09-20 08:56:06 -0700

    correct number 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,20 +4,15 @@
  * more_numbers - Prints the numbers 1 - 14 ten times
  */
 
-void more_numbers(void)
+void print_most_numbers(void)
 {
-	int num, count;
+	int num;
 
-	for (count = 0; count <= 9; count++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (num = 0; num <= 14; num++)
-		{
-			if (num > 9)
-				_putchar((num / 10) + '0');
+		if (num != 2 && num != 4)
 			_putchar((num % 10) + '0');
-		}
-
-		_putchar('\n');
 	}
+	_putchar('\n');
 
 }
