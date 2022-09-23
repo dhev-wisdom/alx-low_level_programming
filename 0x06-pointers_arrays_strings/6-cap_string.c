37cbdd972402e257a357213aace86797bfab84b5
dhev-wisdom <nonsowisdom62@gmail.com>
2022-09-23 09:46:22 -0700

    Correct error

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,6 +3,8 @@
 /**
  * cap_string - capitalize all words of a string
  * @str: The string whose words wre to be capitalized
+ *
+ * Return: The capitalized string.
  */
 
 char *cap_string(char *str)
@@ -10,17 +12,19 @@ char *cap_string(char *str)
 	char delimeters[] = " \t\n,;.!?\"(){}";
 	int i = 0, j;
 
-	for (; str[i] ; i++)
+	for (; str[i]; i++)
 	{
-		if (i == 0 & str[i] >= 'a' && str[i] <= 'z')
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] -= 32;
 		}
 
 		for (j = 0; delimeters[j]; j++)
 		{
-			if (str[i] = delimeters[j]
+			if (str[i] = delimeters[j])
+			{
 				i++;
+			}
 		}
 	}
 
