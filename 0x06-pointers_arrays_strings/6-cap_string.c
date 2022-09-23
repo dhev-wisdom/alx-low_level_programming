a3b53089b3c639db9c25e33a2dba1464ac64507c
dhev-wisdom <nonsowisdom62@gmail.com>
2022-09-23 09:41:48 -0700

    Learn

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize all words of a string
+ * @str: The string whose words wre to be capitalized
+ */
+
+char *cap_string(char *str)
+{
+	char delimeters[] = " \t\n,;.!?\"(){}";
+	int i = 0, j;
+
+	for (; str[i] ; i++)
+	{
+		if (i == 0 & str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+
+		for (j = 0; delimeters[j]; j++)
+		{
+			if (str[i] = delimeters[j]
+				i++;
+		}
+	}
+
+	return (str);
+}
