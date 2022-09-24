e5c844dec2fb701e502371b5240a84ed1f4f3239
dhev-wisdom <nonsowisdom62@gmail.com>
2022-09-24 14:17:14 -0700

    GV

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: The first number to be added
+ * @n2: The second number to be added
+ * @r: Buffer that stores the sum
+ * @size_r: Size of @r
+ *
+ * Return: Pointer to the resukt
+ */
+
+char *infinite_add(char &n1, char *n2, char *r, int size_r)
+{
+
+}
