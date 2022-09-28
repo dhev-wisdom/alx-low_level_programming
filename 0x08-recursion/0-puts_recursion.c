f793135c79f11e5c79ceb29266ea0757853e4784
dhev-wisdom <nonsowisdom62@gmail.com>
2022-09-28 16:10:39 -0700

    Bugfix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,12 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
+	if (s[0] != '\0')
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
 
-	if (s[i] == '\0')
-		return;
-
-	_putchar(s[i]);
-	_puts_recursion(s + 1);
 	_putchar('\n');
 }
