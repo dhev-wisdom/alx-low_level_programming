0066c4c02ff46558cc347ebee019fa7cb9cf0cbd
dhev-wisdom <nonsowisdom62@gmail.com>
2022-09-28 15:50:43 -0700

    Recursion, no loop

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,9 +9,10 @@ void _puts_recursion(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		_putchar(s[i]);
-	}
+	if (s[i] == '\0')
+		return;
+
+	_putchar(s[i]);
+	_puts_recursion(s + 1);
 	_putchar('\n');
 }
