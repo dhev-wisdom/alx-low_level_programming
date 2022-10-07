1f3525bb8d930c29f91895c100f4f3f16c0490d0
dhev-wisdom <nonsowisdom62@gmail.com>
2022-10-06 23:17:16 -0700

    Correct wrongly spelt dir

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two string
+ * @s1: String 1
+ * @s2: String 2
+ *
+ * Return: 1 if @s1 and @s2 are equal
+ * 0 if @s1 and @s2 are not equal
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (s1 == s2)
+	{
+		return (1);
+	}
+}
