ff98105c140b67e130bb2ae8b045431ddf5ad997
dhev-wisdom <nonsowisdom62@gmail.com>
2022-10-21 03:55:28 -0700

    print a sentence before running the program

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - prints a sentence before the main action is executed
+ */
+void first(void)
+{
+	printf("You are beat!, and yet you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
