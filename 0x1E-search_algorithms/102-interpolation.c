f927727d3bd557b5f0046ff9dcef288bcc15a4f2
dhev-wisdom <nonsowisdom62@gmail.com>
2023-04-13 09:32:03 -0700

    Wrong code

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * interpolation_search - searches for a value in a sorted array of integers
+ * using the Interpolation search algorithm
+ * @array: array to search with interpolation search
+ * @size: length of @array
+ * @value: value being searched for
+ *
+ * Return: first index where value is located
+ * Or, -1 if value not in @array or array is NULL
+ */
+
+int interpolation_search(int *array, size_t size, int value)
+{
+	int low = 0, high = size - 1, mid;
+
+	if (value == array[low])
+		return (low);
+
+	while (array[high] != array[low] && value >= array[low] && value <= array[high])
+	{
+		mid = (low + (value - array[low]) * (high - low)) / (array[high] - array[low]);
+
+		printf("Value checked array[%d] = [%d]\n", mid, array[mid]);
+		if (value > array[mid])
+			low = mid + 1;
+		else if (value < array[mid])
+			high = mid - 1;
+		else
+			return (mid);
+	}
+
+	return (-1);
+}
