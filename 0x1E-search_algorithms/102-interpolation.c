41641c85178756c43796719157fcab4fff7c1a3c
dhev-wisdom <nonsowisdom62@gmail.com>
2023-04-13 17:04:38 -0700

    interpolation search algorithm

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -13,22 +13,24 @@
 
 int interpolation_search(int *array, size_t size, int value)
 {
-	int low = 0, high = size - 1, mid;
+	size_t low = 0, high = size - 1, mid;
+
+	if (array == NULL || size == 0)
+		return (-1);
 
 	if (value == array[low])
 		return (low);
 
 	while (array[high] != array[low] && value >= array[low] && value <= array[high])
 	{
-		mid = (low + (value - array[low]) * (high - low)) / (array[high] - array[low]);
-
-		printf("Value checked array[%d] = [%d]\n", mid, array[mid]);
+		mid = low + (value - array[low]) * (high - low) / (array[high] - array[low]);
+		printf("Value checked array[%ld] = [%d]\n", mid, array[mid]);
 		if (value > array[mid])
 			low = mid + 1;
 		else if (value < array[mid])
 			high = mid - 1;
 		else
-			return (mid);
+			return ((int)mid);
 	}
 
 	return (-1);
