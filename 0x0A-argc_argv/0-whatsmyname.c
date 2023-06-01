a6074bca78d1d2ee13ba823fe1e45e6671fe64e5
dhev-wisdom <nonsowisdom62@gmail.com>
2023-06-01 01:47:23 +0100

    argc and argv [0]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * argc: arguments count
+ * argv: arguments list
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+argc and argv
+argc is the number of command line arguments, including the program(name)
+argv is an array of command line arguments
