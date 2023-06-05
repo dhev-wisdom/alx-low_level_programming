8e87527562483e35f9d72615436f7c1c5934296a
dhev-wisdom <nonsowisdom62@gmail.com>
2023-06-05 15:34:14 +0100

    betty

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,7 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		mem = malloc(new_size);
 
-		if(mem == NULL)
+		if (mem == NULL)
 			return (NULL);
 
 		return (mem);
