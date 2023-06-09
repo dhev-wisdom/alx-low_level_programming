a2e8a2c312ba65f2a390aaf8b136dcaa05650833
dhev-wisdom <nonsowisdom62@gmail.com>
2023-06-09 15:16:35 +0100

    Check out

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,6 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * read_textfile - reads a text file and prints it to the standard output
@@ -10,37 +12,45 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int i, y;
+	FILE *fptr;
 	char *buf;
+	ssize_t i;
+	ssize_t y;
 
 	if (!filename)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	fptr = fopen(filename, "r");
+
+	if (fptr == NULL)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
-
-	i = read(fd, buf, letters);
-	if (i < 0)
+	buf = malloc(sizeof(char) * (letters + 1));
+	if (buf == NULL)
 	{
-		free(buf);
+		fclose(fptr);
 		return (0);
 	}
 
-	buf[i] = '\0';
-	close(fd);
+	i = fread(buf, sizeof(char), letters, fptr);
+	if (i < 0)
+	{
+		free(buf);
+		fclose(fptr);
+		return (0);
+	}
+
+	buf[letters] = '\0';
+	
 	y = write(STDOUT_FILENO, buf, i);
 	if (y < 0)
 	{
 		free(buf);
+		fclose(fptr);
 		return (0);
 	}
 
 	free(buf);
+	fclose(fptr);
 	return (y);
 }
