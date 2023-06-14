cb1eb738d7dc5ce5684ad3d6d1b7f06e15b7bf7f
dhev-wisdom <nonsowisdom62@gmail.com>
2023-06-14 03:11:52 +0100

    Check

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,9 @@
 #include "hash_tables.h"
 
+
+hash_node_t *create_item(char *key, char *value);
+
+
 /**
  * hash_table_set - Entry point
  * @ht: hash table
@@ -15,7 +19,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *item = create_item(key, value);
 
 	if (ht->array[index] == NULL)
+	{
 		ht->array[index] = item;
+
+		return (1);
+	}
 	else
 	{
 		hash_node_t *current = ht->array[index];
@@ -25,5 +33,30 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			current = current->next;
 		}
 		current->next = item;
+
+		return (1);
 	}
+
+	return (0);
+}
+
+/**
+ * create_item - creates hash item
+ * @key: key
+ * @value: value
+ *
+ * Return: hash node created
+ */
+hash_node_t *create_item(char *key, char *value)
+{
+	hash_node_t *item = (hash_node_t *) malloc(sizeof(hash_node_t));
+
+	item->key = (char *) malloc(strlen(key) + 1);
+	item->value = (char *) malloc(strlen(value) + 1);
+	item->next = NULL;
+
+	strcpy(item->key, key);
+	strcpy(item->value, value);
+
+	return (item);
 }
