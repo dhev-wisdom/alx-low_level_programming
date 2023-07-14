1db6a1a29cc852f732fc50f8da5ee5c335c8c4a6
dhev-wisdom <nonsowisdom@gmail.com>
2023-07-14 03:08:23 -0700

    strcpy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -29,9 +29,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new_dog->name = name;
+	strcpy(new_dog->name, name);
 	new_dog->age = age;
-	new_dog->owner = owner;
+	strcpy(new_dog->owner, owner);
 
 	return (new_dog);
 
