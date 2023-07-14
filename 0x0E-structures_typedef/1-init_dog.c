dd3264713fab33ea83747edfefabc5893ecc1c99
dhev-wisdom <nonsowisdom@gmail.com>
2023-07-14 00:00:09 -0700

    strcpy

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,8 +15,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		d->name = name;
+		strcpy(d->name, name);
 		d->age = age;
-		d->owner = owner;
+		strcpy(d->owner, owner);
 	}
 }
